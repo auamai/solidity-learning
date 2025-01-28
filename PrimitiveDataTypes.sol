e1da21dfe267c35f087221d8d42be7f92278caf7
auamai <ighodarouamai@gmail.com>
2025-01-27 17:28:40 -0800

    primitive data types in solidity

---
+++ PrimitiveDataTypes.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Primitives {
+    address public newAddr = 0x57031bAEdafC2B192E91E8F94b0394fa9a83C12D;
+    int public neg = -90;
+    uint8 public newU = type(uint8).min;
+}
